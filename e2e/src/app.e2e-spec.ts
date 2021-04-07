085b78d74ed1eb6b6c2d9f24b4fced8f23730c58
Gabriel Jestin <jestingabriel@gmail.com>
2021-04-07 22:51:50 +0200

    Corrige les tests unitaires

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('findo-fe app is running!');
+    expect(await page.getTitleText()).toEqual('Findo app is running!');
   });
 
   afterEach(async () => {
