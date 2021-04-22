5648849105f8540c793059099db25838c56bc7a1
Gabriel Jestin <jestingabriel@gmail.com>
2021-04-22 17:07:13 +0200

    init sidebar + tailwind

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidenavComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SidenavComponent
+  ]
+})
+export class CoreModule { }
